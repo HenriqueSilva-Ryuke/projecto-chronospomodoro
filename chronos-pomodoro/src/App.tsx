23d00392f650a6097d74bb4db2fcd2acf49dedf7
Henrique de Oliveira da Silva <henriqueoliversilvadev@gmail.com>
2026-06-24 11:02:46 +0100

    Subindo arquivos no Repo

---
+++ chronos-pomodoro/src/App.tsx
@@ -0,0 +1,19 @@
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+
+import "./styles/global.css";
+import "./styles/theme.css";
+
+export function App() {
+  return (
+    <>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+    </>
+  );
+}
